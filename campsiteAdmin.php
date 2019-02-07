ed7e90c3282d8533330e07baba4e62e1d4febb68
Afzal <afzaalm059@gmail.com>
2019-02-07 10:53:47 +0000

    Finshed Admin duties without testing

--- campsiteAdmin.php
+++ campsiteAdmin.php
@@ -12,6 +12,8 @@ $userData = new UserDataSet;
 
 $campsiteData = new CampsiteDataSet; 
 
+
+
 // logic for whatever passed in the address bar
 if(isset($_GET['userPage']))
 {
@@ -22,25 +24,33 @@ if(isset($_GET['userPage']))
  // logic for whatever passed in the address bar 
 elseif (isset($_GET['campsitePage']))
 {
-    $view->page= "Admin Campsite"; 
-    $view->campsites = $campsiteData->fetchSomeCampsites($pageNumber, $limit); 
-    require_once 'View/campsiteAdminCampsite.phtml';
+    if (isset($_GET['pageNumber'])) {
 
+        $pageNumber = $_GET['pageNumber']; 
+        $limit= 5;
+        $view->page= "Admin Campsite";
+        $view->campsites = $campsiteData->fetchSomeCampsites($pageNumber, $limit);
+        require_once 'View/campsiteAdminCampsite.phtml';
+    }
 }
 
 elseif(isset($_GET['userID']))
-{
-    $userID = $_GET['userID'];
-    $userData->removeUser($userID); 
-    $view->users = $userData->getAllUserSignedUp(); 
-    require_once 'View/campsiteAdminUser.phtml';
+{   
+        $userID = $_GET['userID'];
+        $userData->removeUser($userID);
+        $view->users = $userData->getAllUserSignedUp();
+        require_once 'View/campsiteAdminUser.phtml';   
 }
 elseif(isset($_GET['campsiteID']))
 {
-    $campsiteID = $_GET['campsiteID']; 
-    $campsiteData->removeCampsite($campsiteID); 
-    $view->campsites = $campsiteData->fetchSomeCampsites($pageNumber, $limit); 
-    require_once 'View/campsiteAdminCampsite.phtml';
+    if (isset($_GET['pageNumber'])) {
+        $pageNumber = $_GET['pageNumber'];
+        $limit= 5;
+        $campsiteID = $_GET['campsiteID'];
+        $campsiteData->removeCampsite($campsiteID);
+        $view->campsites = $campsiteData->fetchSomeCampsites($pageNumber, $limit);
+        require_once 'View/campsiteAdminCampsite.phtml';
+    }
 }
 
 
